a28c7823616e91a66a8ed72b61cf018f93ae44db
unknown <david.powell@ons.gov.uk>
2019-01-30 15:50:38 +0000

    new feature

--- features/steps/change_account.py
+++ features/steps/change_account.py
@@ -6,7 +6,6 @@ from behave import given, when, then
 
 URL = os.getenv('URL', 'http://localhost')
 
-
 @given('there is a customer "{name}"')
 def create_customer(context, name):
     (first_name, surname) = name.split(' ', 2)
@@ -18,8 +17,19 @@ def create_customer(context, name):
     body = response.json()
     context.customer_id = body['customerId']
 
+@when('"{name}" changes his name')
+def create_customer(context, name):
+    (first_name, surname) = name.split(' ', 2)
+    create_customer_request = dict(firstName=first_name, surname=surname)
 
-@given('"{name}" has a new account')
+    response = requests.post(f'{URL}/customers/', json=create_customer_request)
+
+    assert response.status_code == 201, response.status_code
+    body = response.json()
+    context.customer_id = body['customerId']
+
+
+@then('then "{name}" will be created in a new account')
 def create_account(context, name):
     create_account_request = dict(customerId=context.customer_id)
 
@@ -29,30 +39,3 @@ def create_account(context, name):
     assert response.status_code == 201, f'{response.status_code} {response.text}'
     body = response.json()
     context.account_number = body['accountNumber']
-
-
-@when('I deposit {amount:d} the account')
-def deposit(context, amount):
-    deposit_request = dict(accountNumber=context.account_number,
-                           amount=amount,
-                           operation='credit')
-
-    response = requests.post(f'{URL}/cashier/create', json=deposit_request)
-
-    assert response.status_code == 202, f'{response.status_code} {response.text}'
-
-
-@when("I withdraw {amount:d} the account")
-def withdraw(context, amount):
-    deposit_request = dict(accountNumber=context.account_number,
-                           amount=amount,
-                           operation='debit')
-
-    response = requests.post(f'{URL}/cashier/create', json=deposit_request)
-
-    assert response.status_code == 202, f'{response.status_code} {response.text}'
-
-
-
-
-
